ad7eadaad7263a9d5850805493eb268a658b5049
Jose Morales <josemorales-1234@hotmail.es>
2022-05-03 21:38:08 -0400

    Creacion de proyecto

---
+++ cashflow/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <home-component />
+</template>
+
+<script>
+import HomeComponent from "./components/HomeComponent.vue";
+
+export default {
+  components: {
+    HomeComponent,
+  },
+};
+</script>
---
+++ cashflow/src/components/HomeComponent.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class>Hola</div>
+</template>
