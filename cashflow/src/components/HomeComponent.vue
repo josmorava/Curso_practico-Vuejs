434de89af87f85d62abc816e496f1acd1940da05
Jose Morales <josemorales-1234@hotmail.es>
2022-05-06 00:47:32 -0400

    Componente del historial

--- cashflow/src/components/HomeComponent.vue
+++ cashflow/src/components/HomeComponent.vue
@@ -18,7 +18,9 @@
     </template>
 
     <template #movements>
-      <Movements />
+      <Movements
+        :movements="movements"
+      />
     </template>
   </Layout>
 </template>
@@ -27,7 +29,7 @@
 import Layout from "./LayoutC.vue";
 import Header from "./HeaderC.vue";
 import Resume from "./resume/IndexC.vue";
-import Movements from "./MovementsC.vue";
+import Movements from "./movements/IndexC.vue";
 
 export default {
   components: {
@@ -41,7 +43,33 @@ export default {
       amount: null,
       label: "Ahorro total",
       totalLabel: null,
-    };
+      movements: [{
+        id: 0,
+        title: "Movimiento 1",
+        description: "bla bla bla bla bla",
+        amount: 1000,
+      }, {
+        id: 1,
+        title: "Movimiento 2",
+        description: "bla bla bla bla bla",
+        amount: 1000
+      }, {
+        id: 2,
+        title: "Movimiento 3",
+        description: "bla bla bla bla bla",
+        amount: 1000
+      }, {
+        id: 3,
+        title: "Movimiento 4",
+        description: "bla bla bla bla bla",
+        amount: 1000
+      }, {
+        id: 4,
+        title: "Movimiento 5",
+        description: "bla bla bla bla bla",
+        amount: 1000
+      }]
+    }
   },
 };
 </script>
---
+++ cashflow/src/components/movements/IndexC.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="movements">
+    <h2 class="title">Historial</h2>
+    <div class="content">
+      <MovementC 
+      v-for="movement in movements"
+      :key="movement.id"
+      :title="movement.title"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { toRefs, defineProps } from "vue";
+import MovementC from "./MovementC.vue";
+
+const props = defineProps({
+  movements: {
+    type: Array,
+    default: () => [],
+  }
+});
+
+const { movements } = toRefs(props);
+</script>
+
+<style scoped>
+.movements {
+  max-height: 100%;
+  padding: 0 8px;
+  margin-bottom: 14px;
+}
+.title {
+  margin: 8px 16px 24px 16px;
+  color: #3498db;
+}
+.content {
+  max-height: 68vh;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  overflow-y: scroll;
+}
+</style>
